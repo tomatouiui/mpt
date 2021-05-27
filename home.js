2b27e09652b22dab4639a8158064486fea0f1321
Toma <yuzhaok@cn.ibm.com>
2021-05-27 17:09:28 +0800

    2

---
+++ home.js
@@ -0,0 +1,14 @@
+/* globals Chart:false, feather:false */
+
+(function () {
+  'use strict'
+
+  feather.replace()
+
+  // Graphs
+  var ctx = document.getElementById('myChart')
+  
+  function newProject(){
+console.log('aaaaaaaa')
+  }
+})()
